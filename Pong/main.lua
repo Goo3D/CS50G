d1c4801a397d4e2cb1d82e7e28b8d854290ae2a0
Juan <68494966+Goo3D@users.noreply.github.com>
2020-08-04 20:52:46 +0200

    Añadido marcador

--- Pong/main.lua
+++ Pong/main.lua
@@ -1,11 +1,15 @@
--- GD50 Pong Remake V-01
+-- GD50 Pong Remake V-02
+-- Adding a new TextFont
+-- Adding Paddles and ball
+
 
 
 -- Declaraciones simbolicas
 WINDOW_WIDTH = 1280  -- Tamaño de la ventana
 WINDOW_HEIGHT = 720
-VIRTUAL_WIDTH = 432
+VIRTUAL_WIDTH = 432  -- Tamaño virtual de la ventana
 VIRTUAL_HEIGHT = 243
+PADDLE_SPEED = 200 -- Velocidad de las palas
 
 -- push is a library that will allow us to draw our game at a virtual
 -- resolution, instead of however large our window is; used to provide
@@ -22,6 +26,17 @@ function love.load()
 
   love.graphics.setDefaultFilter('nearest', 'nearest')  -- Use nearest filtering
 
+  -- more "retro-looking" font object we can use for any text
+  smallFont = love.graphics.newFont('font.ttf', 8)
+
+  -- larger font for drawing the score on the screen
+  scoreFont = love.graphics.newFont('font.ttf', 32)
+
+
+  -- set LÖVE2D's active font to the smallFont obect
+  love.graphics.setFont(smallFont)
+
+
   -- initialize our virtual resolution, which will be rendered within our
   -- actual window no matter its dimensions; replaces our love.window.setMode call
   -- from the last example
@@ -31,11 +46,46 @@ function love.load()
       vsync = true
   })
 
+  -- initialize score variables, used for rendering on the screen and keeping
+  -- track of the winner
+  player1Score = 0
+  player2Score = 0
 
+  -- paddle positions on the Y axis (they can only move up or down)
+  player1Y = 30
+  player2Y = VIRTUAL_HEIGHT - 50
 
 end
 
 --                 UPDATE CYCLE
+
+
+--[[
+    Runs every frame, with "dt" passed in, our delta in seconds
+    since the last frame, which LÖVE2D supplies us.
+]]
+function love.update(dt)
+    -- player 1 movement
+    if love.keyboard.isDown('w') then
+        -- add negative paddle speed to current Y scaled by deltaTime
+        player1Y = player1Y + -PADDLE_SPEED * dt
+    elseif love.keyboard.isDown('s') then
+        -- add positive paddle speed to current Y scaled by deltaTime
+        player1Y = player1Y + PADDLE_SPEED * dt
+    end
+
+    -- player 2 movement
+    if love.keyboard.isDown('up') then
+        -- add negative paddle speed to current Y scaled by deltaTime
+        player2Y = player2Y + -PADDLE_SPEED * dt
+    elseif love.keyboard.isDown('down') then
+        -- add positive paddle speed to current Y scaled by deltaTime
+        player2Y = player2Y + PADDLE_SPEED * dt
+    end
+end
+
+
+
 
 
 -- Keyboard handling, called by LÖVE2D each frame;
@@ -57,9 +107,29 @@ function love.draw()
 
 
   push:apply('start')    -- begin rendering at virtual resolution
+  love.graphics.clear(.15, .15, .15, 1)    --clear screen with a spicific color
 
-  -- note we are now using virtual width and height now for text placement
-  love.graphics.printf('Hello Pong!', 0, VIRTUAL_HEIGHT / 2 - 6, VIRTUAL_WIDTH, 'center')
+  -- draw welcome text toward the top of the screen
+  love.graphics.setFont(smallFont)
+  love.graphics.printf('Hello Pong!', 0, 20, VIRTUAL_WIDTH, 'center')
+
+
+  -- draw score on the left and right center of the screen
+  -- need to switch font to draw before actually printing
+  love.graphics.setFont(scoreFont)
+  love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50,
+      VIRTUAL_HEIGHT / 3)
+  love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30,
+      VIRTUAL_HEIGHT / 3)
+
+
+
+
+  -- paddles and ball are simply rectangles we draw on the screen.
+
+  love.graphics.rectangle('fill', 10, 30, 5, 20)  -- first paddle (left side)
+  love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20)  -- second paddle (right side)
+  love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)  -- ball (center)
 
 
   push:apply('end')  -- end rendering at virtual resolution
